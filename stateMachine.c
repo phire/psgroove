91ce963254a94ba811ea6c2ed8a2049c98119a53
Scott Mansell <phiren@gmail.com>
2010-10-18 04:18:04 +1300

    Improved the desgin of the state machine.
    I'm not sure if its even possible to get this to compile in c, but I hope so.

--- stateMachine.c
+++ stateMachine.c
@@ -15,26 +15,51 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "devices.h"
+
 device active_device = hub_device;
+int current_state = 0;
 
-enum { 
+// The states that our state machine will transistion through
+const PROGMEM state states[] = {
 	init,
-	hub_connect,
-	p1_connect,
-	p2_connect,
-	p3_connect,
-	p2_disconnect,
-	p4_connect,
-	p5_connect,
-        jig_challange,
-	p3_disconnect,
-	p5_disconnect,
-	p4_disconnect,
-	p1_disconnect,
+	power_hub_ports,
+	wait(15),
+	connect_port(1, port1_device),
+	wait(10),
+	connect_port(2, port2_device),
+	wait(15),
+	connect_port(3, port3_device),
+	wait(10),
+	disconnect_port(2),
+	wait(15),
+	connect_port(4, port4_device),
+	wait(20),
+	connect_port(5, port5_device),
+	receive_challange,
+	wait(50),
+	send_response,
+	wait(15),
+	disconnect_port(3),
+	wait(45),
+	disconnect_port(5),
+	wait(20),
+	disconnect_port(4),
+	wait(20),
+	disconnect_port(1),
 	done,
-} state = init_hub;
-
+};
 
 void advance_state() {
-
+	state = states[current_state++];
+	switch(state.type) {
+	case wait:
+		set_timeout(state.length);
+	case connect_port:
+		hub_connect(state.port, state.device);
+	case disconnect_port:
+		hub_disconnect(state.port);
+	case function:
+		state.function();
+	}
 }
